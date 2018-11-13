45894cab8b8415a9524d9e7d075eaa87551bdb08
vagrant <vagrant@vagrant.vm>
2018-11-13 07:09:04 +0000

    Solve multi-threading issue.

--- new_application.py
+++ new_application.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from new_db import Base, Department, Instrument
+from flask_sqlalchemy import SQLAlchemy
+from new_db import Base, Department, Instrument, db_session
 app = Flask(__name__)
-
 
 engine = create_engine('sqlite:///internalinstrumentlibrary.db')
 Base.metadata.bind = engine
@@ -11,18 +11,66 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/departments/<int:department_id>/instrument_list/JSON')
+def departmentJSON(department_id):
+    department = session.query(Department).filter_by(id=department_id).one()
+    instruments = session.query(Instrument).filter_by(department_id=department_id).all()
+    return jsonify(Instrument=[i.serialize for i in instruments])
 
-@app.route('/')
-@app.route('/hello')
-def addDepartment():
-    department = session.query(Department).first()
-    instruments = session.query(Instrument).filter_by(department_id=department.id)
+@app.route('/departments/')
+def departmentList():
+    department = session.query(Department).all()
     output = ''
-    for i in instruments:
+    for i in department:
         output += i.name
         output += '</br>'
     return output
 
+@app.route('/departments/<int:department_id>/')
+def populateDepartment(department_id):
+    department = session.query(Department).filter_by(id = department_id).one()
+    instruments = session.query(Instrument).filter_by(department_id = department_id)
+    return render_template('instrument.html', department = department, instruments = instruments)
+
+#Function to add an instrument to the library
+@app.route('/departments/<int:department_id>/new', methods = ['GET', 'POST'])
+def newInstrument(department_id):
+    if request.method == 'POST':
+        newInstrument = Instrument(name=request.form['name'],description=request.form['description'], instrument_family=request.form['instrument_family'], owner=request.form['owner'], dollar_value=request.form['dollar_value'], department_id=department_id)
+    # 
+        session.add(newInstrument)
+        session.commit()
+        return redirect(url_for('populateDepartment', department_id=department_id))
+    else:
+        return render_template('new_instrument.html', department_id=department_id)
+
+#Function to edit an instrument in the library
+@app.route('/departments/<int:department_id>/<int:instrument_id>/edit', methods=['GET', 'POST'])
+def editInstrument(department_id, instrument_id):
+    editedInstrument = session.query(Instrument).filter_by(id=instrument_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedInstrument.name = request.form['name']
+        session.add(editedInstrument)
+        session.commit()
+        return redirect(url_for('populateDepartment', department_id=department_id))
+    else:
+        return render_template('editInstrument.html', department_id=department_id, instrument_id=instrument_id, item=editedInstrument)
+
+@app.route('/departments/<int:department_id>/<int:instrument_id>/delete', methods=['GET', 'POST'])
+def deleteInstrument(department_id, instrument_id):
+    itemToDelete = session.query(Instrument).filter_by(id=instrument_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('populateDepartment', department_id=department_id))
+    else:
+        return render_template('deleteinstrument.html', item=itemToDelete)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, threaded=False)
--- new_db.py
+++ new_db.py
@@ -2,10 +2,14 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker
 from sqlalchemy import create_engine
 
+engine = create_engine('sqlite:///internalinstrumentlibrary.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine)) #Scoped session code from Flask for multi-threading
+
 Base = declarative_base()
+Base.query = db_session.query_property()
 
 
 class Department(Base):
@@ -27,6 +31,16 @@ class Instrument(Base):
     department_id = Column(Integer, ForeignKey('department.id'))
     department = relationship(Department)
 
-engine = create_engine('sqlite:///internalinstrumentlibrary.db')
+    @property
+    def serialize(self):
+
+        return {
+            'name': self.name,
+            'id': self.id,
+            'description': self.description,
+            'instrument_family': self.instrument_family,
+            'owner': self.owner,
+            'dollar_value': self.dollar_value,
+        }
 
 Base.metadata.create_all(engine)
